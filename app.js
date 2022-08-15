c4420de052c7540e08361f539af6a48fe5ecec94
nanducosmic <nanducodes@gmail.com>
2022-08-16 01:25:26 +0530

    dom selectors

---
+++ app.js
@@ -0,0 +1,4 @@
+const gameboard=document.querySelector('#gameboard')
+const ctx=gameboard.getcontext("2d")
+const scoreText=document.querySelector('scoreText')
+const resetBtn=document.querySelector('resetBtn')
